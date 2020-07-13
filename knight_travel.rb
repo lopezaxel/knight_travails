7912601bbece8cb58e565b0ed5a395f4681c6102
Axel Lopez <lopezaxel@protonmail.com>
2020-07-13 13:46:06 -0300

    Refactored code

--- knight_travel.rb
+++ knight_travel.rb
@@ -1,7 +1,5 @@
-require 'pry'
-
 class Gameboard
-  attr_accessor :board, :knight
+  attr_accessor :board
 
   def initialize
     @board = create_board
@@ -9,7 +7,6 @@ class Gameboard
 
   def create_board
     Array.new(8) { Array.new(8, []) }
-    # fill_board(board)
   end
 end
 
@@ -27,8 +24,6 @@ class Knight
     if row >= 0 && row <= 7 && col >= 0 && col <= 7
       moves << square 
     end
-
-    moves
   end
 
   def moves(square)
@@ -51,36 +46,36 @@ class Knight
   def knight_moves(start_sq, end_sq)
     path = []
     path << end_sq
+
     until path.first == start_sq
-      previous = bfs(start_sq, path.first)
+      previous = find_previous_move(start_sq, path.first)
       path.unshift(previous)
     end
 
-    path
+    print_path(path)
   end
 
-  def bfs(start_sq, end_sq)
+  def print_path(moves)
+    puts "\nYou made it in #{moves.length - 1} moves!. Here's your path:"
+
+    moves.each { |square| p square }
+  end
+
+  def find_previous_move(start_sq, end_sq)
     queue = []  
     queue << start_sq
 
-    visited = Array.new(8) {Array.new(8, false)}
     path = gameboard.create_board
 
-    visited[start_sq[0]][start_sq[1]] = true
     until queue.empty?
       sq = queue.shift 
       possible_moves = moves(sq)
      
       possible_moves.each do |move|
-        if !visited[move[0]][move[1]]
-          if move == end_sq
-            path[move[0]][move[1]] = sq
-            return path[move[0]][move[1]]
-          end
-          queue << move
-          visited[move[0]][move[1]] = true
-          path[move[0]][move[1]] = sq
-        end
+        queue << move
+        path[move[0]][move[1]] = sq
+
+        return path[move[0]][move[1]] if move == end_sq
       end
     end
   end
@@ -89,6 +84,9 @@ end
 gameboard = Gameboard.new
 knight = Knight.new(gameboard)
 
-p knight.knight_moves([3, 3], [4, 3])
-p knight.knight_moves([0, 0], [7, 7])
+knight.knight_moves([0, 0], [1, 2])
+knight.knight_moves([0, 0], [3, 3])
+knight.knight_moves([3, 3], [0, 0])
+knight.knight_moves([3, 3], [4, 3])
+knight.knight_moves([0, 0], [7, 7])
 
